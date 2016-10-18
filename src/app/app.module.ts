19c1bb1f5268a365ccebdd4b4ed070cb87ad7e1a
Sino Thomas <sinothomas9@gmail.com>
2016-10-18 10:30:18 -0400

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { Page1 } from '../pages/page1/page1';
+import { Page2 } from '../pages/page2/page2';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    Page1,
+    Page2
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    Page1,
+    Page2
+  ],
+  providers: []
+})
+export class AppModule {}
