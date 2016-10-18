9d92f4694cd4c848247d8ce4d70d07e3e137f9ce
Sino Thomas <sinothomas9@gmail.com>
2016-10-18 11:45:30 -0400

    Added Component: circularTabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,30 @@ import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+import { CircularTabs } from '../components/circular-tabs/circular-tabs';
+
 import { LandingPage } from '../pages/landing-page/landing-page';
 import { TabsBottom } from '../pages/tabs-bottom/tabs-bottom';
 import { TabsBottom2 } from '../pages/tabs-bottom2/tabs-bottom2';
 import { TabsRight } from '../pages/tabs-right/tabs-right';
 import { TabsRight2 } from '../pages/tabs-right2/tabs-right2';
+import { Home } from '../pages/home/home';
+import { Calendar } from '../pages/calendar/calendar';
+import { Camera } from '../pages/camera/camera';
+import { Cloud } from '../pages/cloud/cloud';
+import { Compas } from '../pages/compas/compas';
+import { Contact } from '../pages/contact/contact';
+import { Folder } from '../pages/folder/folder';
+import { Images } from '../pages/images/images';
+import { Map } from '../pages/map/map';
+import { Settings } from '../pages/settings/settings';
 
 @NgModule({
   declarations: [
     MyApp,
-    LandingPage,
-    TabsBottom,
-    TabsBottom2,
-    TabsRight,
-    TabsRight2
+    CircularTabs,
+    LandingPage, TabsBottom, TabsBottom2, TabsRight, TabsRight2,
+    Home, Calendar, Camera, Cloud, Compas, Contact, Folder, Images, Map, Settings
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -23,12 +33,8 @@ import { TabsRight2 } from '../pages/tabs-right2/tabs-right2';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    LandingPage,
-    TabsBottom,
-    TabsBottom2,
-    TabsRight,
-    TabsRight2
+    LandingPage, TabsBottom, TabsBottom2, TabsRight, TabsRight2
   ],
   providers: []
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/components/circular-tabs/circular-tabs.ts
@@ -0,0 +1,151 @@
+import { Component, ElementRef, Renderer, ViewChild, Input, OnChanges, SimpleChanges, AfterViewInit, trigger, state, style, transition, animate } from '@angular/core';
+import { Tabs } from 'ionic-angular';
+
+@Component({
+  selector: 'circular-tabs',
+  templateUrl: 'circular-tabs.html',
+  host: {
+    '(document:click)': 'onClick($event)',
+  },
+  animations: [
+    trigger('menu', [
+      state('open', style({
+        transform: 'scale(1)',
+        opacity: '1'
+      })),
+      state('close', style({
+        transform: 'scale(0.3)',
+        opacity: '0'
+      })),
+      transition('close => open', animate('300ms ease-in')),
+      transition('open => close', animate('300ms ease-out'))
+    ])
+  ]
+})
+
+export class CircularTabs implements OnChanges, AfterViewInit {
+
+  @ViewChild('list') listRef: ElementRef;
+  @ViewChild('menuButton') menuButtonRef: ElementRef;
+
+  @Input() tabRef: Tabs;
+  @Input() totalAngle: number = 180;
+  @Input() gapAngle: number = 2;
+  @Input() startAngle: number = -1;
+  @Input() closeOnTabSelect: boolean = true;
+  @Input() closedBtnText: string = "Menu";
+  @Input() openedBtnText: string = "Close";
+
+  isNavOpened: boolean = false;
+  positionXLock: boolean = false;
+  positionYLock: boolean = false;
+
+
+  constructor(public elemRef: ElementRef, public renderer: Renderer) {
+
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // if (changes['positionLock'].currentValue != changes['positionLock'].previousValue) { }
+    this.styleMenu();
+
+  }
+
+  ngAfterViewInit() {
+    // hide original tabbar
+    this.tabRef._tabbar.nativeElement.hidden = true;
+
+    setTimeout(() => {
+      if (this.positionXLock) {
+        this.lockElemPositionX();
+      }
+      if (this.positionYLock) {
+        this.lockElemPositionY();
+      }
+    }, 2000);
+
+    this.styleMenu();
+
+
+    // for (var i = 0; i < 10; i ++) {
+    //   setTimeout(() => {
+    //     this.totalAngle = 180+i;
+    //     this.styleMenu();
+    //   }, 1000*i);
+    // }
+
+  }
+
+  lockElemPositionY() {
+    let top = window.getComputedStyle(this.elemRef.nativeElement, null).getPropertyValue("top");
+    this.renderer.setElementStyle(this.elemRef.nativeElement, 'top', top);
+  }
+
+  lockElemPositionX() {
+    let left = window.getComputedStyle(this.elemRef.nativeElement, null).getPropertyValue("left");
+    this.renderer.setElementStyle(this.elemRef.nativeElement, 'left', left)
+
+  }
+
+  selectTab(tabIndex) {
+    this.tabRef.selectedIndex = tabIndex;
+    this.changeTab();
+    if (this.closeOnTabSelect) {
+      this.toggleNav();
+    }
+  }
+
+  changeTab() {
+    this.tabRef.select(this.tabRef.selectedIndex);
+  }
+
+  toggleNav() {
+    this.isNavOpened ? this.closeNav() : this.openNav();
+  }
+
+  closeNav() {
+    this.isNavOpened = false;
+    console.log('navclosed');
+
+  }
+
+  openNav() {
+    this.isNavOpened = true;
+    console.log('navopened');
+
+  }
+
+  styleMenu() {
+    let numofTabs = this.tabRef._tabs.length;
+    let centralAngel = (this.totalAngle / numofTabs) - (((numofTabs) * this.gapAngle) / numofTabs);
+    let startAngle = this.startAngle >= 0 ? this.startAngle : ((180 - this.totalAngle) / 2);
+    let tabbar_bgColor = window.getComputedStyle(this.tabRef._tabbar.nativeElement, null).getPropertyValue("background-color");
+    let a_bgColor = tabbar_bgColor.indexOf('a') == -1 ? tabbar_bgColor.replace(')', ', 1)').replace('rgb', 'rgba') : console.log('rbg to rbga conversion error');  //convert rbg to rbga
+
+    for (var i = 0; i < this.tabRef._tabs.length; i++) {
+      if (typeof (this.listRef.nativeElement.children[i]) != 'undefined' && this.listRef.nativeElement.children[i] != null) {
+        //li
+        this.renderer.setElementStyle(this.listRef.nativeElement.children[i], "transform", "rotate(" + ((i * centralAngel) + startAngle + (i * this.gapAngle)) + "deg) skew(" + (90 - centralAngel) + "deg)");
+        //a
+        let tabbar_color = window.getComputedStyle(this.tabRef._tabbar.nativeElement.children[i], null).getPropertyValue("color");
+        this.renderer.setElementStyle(this.listRef.nativeElement.children[i].children[0], 'color', tabbar_color);
+        this.renderer.setElementStyle(this.listRef.nativeElement.children[i].children[0], 'background', "radial-gradient(transparent 35%, " + a_bgColor + " 35%)");
+        this.renderer.setElementStyle(this.listRef.nativeElement.children[i].children[0], 'transform', "skew(" + ((90 - centralAngel) * -1) + "deg) rotate(" + ((centralAngel / 2) - 90) + "deg)");
+      }
+    }
+    // button
+    this.renderer.setElementStyle(this.menuButtonRef.nativeElement, 'background-color', tabbar_bgColor);
+  }
+
+  onClick(event) {
+    console.log('clicked outside');
+
+    // check if clicked outside this element
+    if (!this.elemRef.nativeElement.contains(event.target)) {
+      this.closeNav();
+      console.log('clicked outside closing Nav...');
+    }
+
+  }
+
+}
